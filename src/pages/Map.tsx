3ad53e53babeb18c6953e6b24b589510e4d92a94
nightapple <nightapple126@gmail.com>
2022-06-15 00:44:29 +0800

    react-antd-1

---
+++ src/pages/Map.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Header } from 'src/components/Map/MapHeader';
+import { Map } from 'src/components/Map';
+import { Helmet } from 'react-helmet-async';
+
+const _MapPage = () => {
+  const [eventData, setEventData] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await fetch(
+        'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events',
+      );
+      const { events } = await res.json();
+      setEventData(events);
+    };
+    fetchEvents();
+  }, [eventData]);
+
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <title>Map | React Antd Demo</title>
+        <meta name="description" content="Google map React demo" />
+      </Helmet>
+
+      <Header />
+      <Map eventData={eventData} />
+    </div>
+  );
+};
+
+const MapPage = React.memo(_MapPage);
+export default MapPage;
